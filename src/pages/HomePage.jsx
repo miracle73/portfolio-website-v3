83ebf1a5caf41980ca9d0201cc1a41d3f7b6bef6
miracle73 <50253665+miracle73@users.noreply.github.com>
2023-12-13 23:31:20 +0100

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='min-h-screen w-full p-0 border-4'>
+      <div className=' h-[30rem] bg-gray-200'>
+        <p>Are you okay</p>
+      </div>
+      <p>Are you okay</p>
+    </div>
+  )
+}
+
+export default HomePage
