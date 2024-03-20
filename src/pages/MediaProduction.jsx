8e43ed832ad262f3741c0c7569ccc5a0b90e08c1
miracle73 <50253665+miracle73@users.noreply.github.com>
2024-03-20 23:19:47 +0100

    change effected

--- src/pages/MediaProduction.jsx
+++ src/pages/MediaProduction.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import BackgroundImage from '../assets/images/background-image.jpg'
 import NavLinks from '../components/NavLinks'
 import firstImage from '../assets/images/mediaProduction.png'
@@ -14,10 +14,10 @@ const MediaProduction = () => {
     const [nav, setNav] = useState(false)
     return (
         <div className='min-h-screen w-full p-0'>
-           
+
             <div style={{ backgroundImage: `url(${BackgroundImage})` }}
                 className="bg-cover bg-center h-[15rem] w-full relative">
-               {nav && <div className='w-full h-full flex items-center justify-center absolute'>
+                {nav && <div className='w-full h-full flex items-center justify-center absolute'>
                     <MobileNav setNav={setNav} />
                 </div>}
                 {!nav && <div className='lg:pt-10 max-lg:pt-14 max-md:pt-5 max-md:flex max-md:items-center max-md:justify-end max-md:pr-10 '>
@@ -28,33 +28,44 @@ const MediaProduction = () => {
             <div className="  w-full flex flex-col pt-10 bg-black ">
                 <div className='flex items-start justify-between px-16 max-lg:px-10 max-md:px-7 max-sm:px-4 mt-10 '>
                     <div className=' w-full '>
-                        <div className=" text-white text-[32px] max-sm:text-2xl font-normal font-['Javanese Text']">Media Production</div>
+                        <div className=" text-white text-[32px] max-sm:text-2xl font-normal font-['Javanese Text']"> Media and Content Production </div>
                         <img src={firstImage} className='mt-5 w-full h-96 max-lg:h-86 max-md:72 max-sm:h-64 object-cover' />
-                        <div className=" text-white text-[20px] max-sm:text-sm font-normal font-['Javanese Text'] mt-5">Elevate your event to the next level with my professional Event Host services.
-                            With my expertise in orchestrating the event flow and fostering audience engagement, I guarantee a memorable event that leaves a lasting impression. Do you check me out</div>
+                        <div className=" text-white text-[20px] max-sm:text-sm font-normal font-['Javanese Text'] mt-5"> With over 15 years of experience in Radio Broadcasting, Advertising, Audio Production, video production, and TV/Film production, I bring a wealth of expertise and creativity to every project I undertake. My media and content production services are tailored to meet the unique needs of my clients.
+                        </div>
 
                     </div>
 
                 </div>
-                <div className=" text-white text-[32px] max-sm:text-2xl font-normal font-['Javanese Text'] px-16 max-lg:px-10 max-md:px-7 max-sm:px-4 mt-40 max-lg:mt-32 max-md:mt-24 max-sm:scroll-mt-16">Previous works</div>
-                <div className='flex items-start justify-between px-16 max-lg:px-10 max-md:px-7 max-sm:px-4 mt-5'>
-                    <div className='w-[50.0%] h-72 hover:cursor-pointer flex hover:bg-blend-multiply hover:bg-gray-500 items-end pb-2 hover:pb-4 justify-center' style={{ backgroundImage: `url(${secondImage})` }}>
-                    <div className=" text-white text-2xl max-md:text-xl max-sm:text-lg font-normal font-['Javanese Text']">Wizkid Concert</div>
+
+                <div className=" text-white text-center text-[32px] max-md:text-2xl max-sm:text-xl font-normal font-['Javanese Text'] px-16 mt-24 "> Services Offered </div>
+                <div className='flex items-start justify-between px-16 max-lg:px-10 max-md:px-7 max-sm:px-4 mt-16 '>
+                    <div className='w-[33%] '>
+                        <div className='w-16 h-16 max-md:w-12 max-md:h-12 max-sm:w-8 max-sm:h-8 rounded-full bg-white items-center flex justify-center'>
+                            <img src={secondImage} className='max-sm:w-6 max-md:w-8 max-lg:w-10 max-xsm:w-5' />
+                        </div>
+                        <div className=" text-white text-2xl max-lg:text-xl max-md:text-lg max-sm:text-base font-normal font-['Inter'] mt-5"> Audio Production </div>
+                        <div className=" text-white text-lg max-lg:text-base max-md:text-sm max-sm:text-xs font-light font-['Inter']"> From radio commercials, radio shows, podcasts, and voiceovers to sound design. I have a keen ear for detail and a passion for storytelling, ensuring that every audio project is crafted to perfection, leaving a lasting impact on listeners.
+                        </div>
+
                     </div>
-                    <div className='w-[50.0%] h-72 hover:cursor-pointer flex hover:bg-blend-multiply hover:bg-gray-500 items-end pb-2 hover:pb-4 justify-center'  style={{ backgroundImage: `url(${thirdImage})` }}>
-                    <div className=" text-white text-2xl max-md:text-xl max-sm:text-lg  font-normal font-['Javanese Text']">Pulse Naija</div>
+                    <div className='w-[33%] '>
+                        <div className='w-16 h-16 max-md:w-12 max-md:h-12 max-sm:w-8 max-sm:h-8 rounded-full  bg-white items-center flex justify-center'>
+                            <img src={thirdImage} className='max-sm:w-6 max-md:w-8 max-lg:w-10 max-xsm:w-5' />
+                        </div>
+                        <div className=" text-white text-2xl max-lg:text-xl max-md:text-lg max-sm:text-base font-normal font-['Inter'] mt-5"> Video Production </div>
+                        <div className=" text-white text-lg max-lg:text-base max-md:text-sm max-sm:text-xs font-light font-['Inter']"> From concept development to post-production, I offer comprehensive video production services for a variety of purposes, including promotional videos, short-form videos, and documentaries. Using visual storytelling, I bring your ideas to life on screen with creativity and precision. </div>
                     </div>
-                  
+                    <div className='w-[33%] '>
+                        <div className='w-16 h-16 max-md:w-12 max-md:h-12 max-sm:w-8 max-sm:h-8 rounded-full  bg-white items-center flex justify-center'>
+                            <img src={thirdImage} className='max-sm:w-6 max-md:w-8 max-lg:w-10 max-xsm:w-5' />
+                        </div>
+                        <div className=" text-white text-2xl max-lg:text-xl max-md:text-lg max-sm:text-base font-normal font-['Inter'] mt-5"> TV/Film Production </div>
+                        <div className=" text-white text-lg max-lg:text-base max-md:text-sm max-sm:text-xs font-light font-['Inter']"> My expertise in TV/Film production spans various genres, including commercials, short films, and documentaries. Whether you're looking to create captivating content for television, streaming platforms, or the big screen, my hands-on approach and attention to detail ensure that your project is executed flawlessly from start to finish.
+
+                        </div>
+                    </div>
                 </div>
-                <div className='flex items-start justify-between px-16 max-lg:px-10 max-md:px-7 max-sm:px-4 mt-5'>
-                    <div className='w-[50.0%] h-72 hover:cursor-pointer flex hover:bg-blend-multiply hover:bg-gray-500 items-end pb-2 hover:pb-4 justify-center' style={{ backgroundImage: `url(${fourthImage})` }}>
-                    <div className=" text-white text-2xl max-md:text-xl max-sm:text-lg  font-normal font-['Javanese Text']">Wizkid Concert</div>
-                    </div>
-                    <div className='w-[50.0%] h-72 hover:cursor-pointer flex hover:bg-blend-multiply hover:bg-gray-500 items-end pb-2 hover:pb-4 justify-center'  style={{ backgroundImage: `url(${fifthImage})` }}>
-                    <div className=" text-white text-2xl max-md:text-xl max-sm:text-lg  font-normal font-['Javanese Text']">Pulse Naija</div>
-                    </div>
-                  
-                </div>
+               
                 <div className='flex justify-center items-center mt-16'>
                     <button className=" h-8 px-4 py-4 bg-white rounded-[32px] justify-start items-center gap-4 inline-flex mt-10">
                         <div className="text-center text-black text-lg max-md:text-base max-sm:text-xs font-normal font-['Jacques Francois']">Book now</div>
